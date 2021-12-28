d8fa226d29c4bfe85c2b7fba2bb42377e22ba4b6
negin <a@b.com>
2021-12-28 14:16:30 +0330

    all solution to overlapping question

---
+++ Algorithms/Algorithms/Algorithms/FindOverlapping.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Algorithms.Algorithms
+{
+    /*Quesstion : Find overlapping
+         Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+         Output: [[1,6],[8,10],[15,18]]
+         Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].*/
+    public static class Overlapping
+    {
+        //Time Complexity : o(n)  
+        //Space Complexity : o(n)
+        public static int[][] Merge(int[][] intervals)
+        {
+            if (intervals.Length == 1 || intervals == null) return intervals;
+            intervals = intervals.OrderBy(x => x[0]).ToArray();
+            var current = intervals[0];
+            List<int[]> mergedIntervals = new List<int[]>() { };
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                if (current[1] >= intervals[i][0])
+                {
+                    current = new int[2] { current[0], Math.Max(current[1], intervals[i][1]) };
+                }
+                else
+                {
+                    mergedIntervals.Add(current);
+                    current = intervals[i];
+                }
+            }
+            mergedIntervals.Add(current);
+            return mergedIntervals.ToArray();
+        }
+    }
+}
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algorithms.Algorithms;
+using System;
 
 namespace Algorithms
 {
@@ -6,7 +7,15 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1- Find Overlapping
+            var intervals = new int[3][];
+            intervals[0] = new int[2] { 1, 4 };
+            intervals[1] = new int[2] { 0, 2 };
+            intervals[2] = new int[2] { 3, 5 };
+            var res = Overlapping.Merge(intervals);
+
+
+            Console.ReadLine();
         }
     }
 }
