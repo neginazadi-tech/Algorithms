134d8c55caeaab6f8c76d969bab56ec8533051b9
negin <a@b.com>
2021-12-28 19:40:29 +0330

    a solution to 'Adding2LinkedList'

---
+++ Algorithms/Algorithms/Algorithms/Adding2LinkedList.cs
@@ -0,0 +1,41 @@
+﻿namespace Algorithms.Algorithms
+{
+    /*
+        Input: l1 = [2,4,3], l2 = [5,6,4] => they are linked List
+        Output: [7,0,8]
+        Explanation: 342 + 465 = 807.
+     */
+    public static class Adding2LinkedList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        //Solution : We have to sum coresponding value in 2 linked list.
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode(0), current = result; int leftover = 0;
+            while (l1 != null || l2 != null)
+            {
+                if (l1 == null) l1 = new ListNode(0);
+                if (l2 == null) l2 = new ListNode(0);
+
+                int sum = leftover + l1.val + l2.val;
+                int mod = sum % 10;
+                leftover = sum / 10;
+                current.next = new ListNode(mod);
+                current = current.next;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+            if (leftover > 0) current.next = new ListNode(leftover);
+            return result.next;
+        }
+    }
+}
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,13 +1,14 @@
 ﻿using Algorithms.Algorithms;
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace Algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //1- Find Overlapping
+            //1- Find Overlapping in a given array
             var intervals = new int[3][];
             intervals[0] = new int[2] { 1, 4 };
             intervals[1] = new int[2] { 0, 2 };
@@ -17,6 +18,24 @@ namespace Algorithms
             //2- Find Longest Substring
             var longest1 = LongestSubstring.LengthOfLongestSubstring("abcabccdefg");
             var longest2 = LongestSubstring.LengthOfLongestSubstring("abcabccdefg");
+
+            //3- You are given two non-empty linked lists representing two non-negative integers. 
+            //The digits are stored in reverse order, and each of their nodes contains a single digit. 
+            //Add the two numbers and return the sum as a linked list.
+            var l1 = new Adding2LinkedList.ListNode(9,
+                new Adding2LinkedList.ListNode(9,
+                new Adding2LinkedList.ListNode(9,
+                new Adding2LinkedList.ListNode(9,
+                new Adding2LinkedList.ListNode(9,
+                new Adding2LinkedList.ListNode(9,
+                new Adding2LinkedList.ListNode(9, null)))))));
+
+            var l2 = new Adding2LinkedList.ListNode(9,
+                new Adding2LinkedList.ListNode(9,
+                new Adding2LinkedList.ListNode(9,
+                new Adding2LinkedList.ListNode(9, null))));
+
+            var funalLinkedList = Adding2LinkedList.AddTwoNumbers(l1, l2);
 
             Console.ReadLine();
         }
